179aba29e4f5cd67b6197cb1746069e501059112
ReaganBrian <reagansiahaan2006@gmail.com>
2025-11-02 00:30:13 +0700

    ketinggalan 1 tombol dipasangin route

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard Siswa</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap');
+    html, body, * { font-family: 'Poppins', sans-serif !important; }
+    </style>
     
 </head>
 <body class="bg-slate-100 font-sans">
