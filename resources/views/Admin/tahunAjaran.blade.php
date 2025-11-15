942508dc3cbcac708381fe91822fe8b51c50feff
andrefarizi <andrefarizi@users.noreply.github.com>
2025-11-15 12:21:17 +0700

    Membuat page pendataan siswa dan guru

--- resources/views/Admin/tahunAjaran.blade.php
+++ resources/views/Admin/tahunAjaran.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.admin.app')
 
+@section('title', 'Tahun Ajaran')
+
 @section('content')
 <div class="content-wrapper p-6">
 
--- resources/views/layouts/guru/app.blade.php
+++ resources/views/layouts/guru/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard Siswa</title>
+    <title>@yield('title', 'Dashboard Guru') | SMAN 12 Connect</title>
     @vite('resources/css/app.css')
     <script src="https://cdn.tailwindcss.com"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
